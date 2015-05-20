f55082a5f4e583b5cc80074a5677a35f35edbfa6
Michael <michaelgarder@outlook.com>
2015-05-20 14:55:33 +0200

    Side til at se reservationer på nuværende account

---
+++ public/app/viewTickets/viewTickets.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Michael on 20/05/15.
+ */
+'use strict';
+
+angular.module('myAppRename.viewTickets', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/viewTickets', {
+            templateUrl: 'app/viewTickets/viewTickets.html',
+            controller: 'ViewTicketsCtrl'
+        });
+    }])
+
+    .controller('ViewTicketsCtrl', function ($scope, $http) {
+
+        $http({
+            method: 'GET',
+            url: 'userApi/test'
+        })
+            .success(function (data, status, headers, config) {
+                $scope.info = data;
+                $scope.error = null;
+                $http({
+                    method: 'GET',
+                    url: 'userApi/tickets/michael'
+                })
+                    .success(function(data, status,headers,config){
+
+                        if(data.constructor === Array && typeof data[0] !== 'undefined'){
+
+                            $scope.tickets=data;
+                            $scope.noTicketsFound=false;
+                        }else{
+
+                            $scope.noTicketsFound=true;
+                        }
+                    })
+                    .error(function(data,status,headers,config){
+
+                        $scope.error = 'An error occured getting your reservations';
+                    });
+            }).
+            error(function (data, status, headers, config) {
+                if (status == 401) {
+                    $scope.error = "You are not authenticated to request these data";
+                    return;
+                }
+                $scope.error = data;
+            });
+    });
+
